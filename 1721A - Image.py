e06590267c7db1485c68fb9bb32312dfc972e497
Abhay-codes <97458191+Abhay-codes@users.noreply.github.com>
2022-09-27 02:30:56 +0530

    Create 1721A - Image.py

---
+++ 1721A - Image.py
@@ -0,0 +1,11 @@
+#https://codeforces.com/problemset/problem/1721/A
+#from collections import defaultdict
+for _ in range(int(input())):
+    
+    s1=input()
+    s2=input()
+    s1=s1+s2 
+    s=set(s1)
+    print(len(s)-1)
+    
+   
